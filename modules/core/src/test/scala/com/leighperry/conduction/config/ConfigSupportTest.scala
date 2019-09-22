1e670dd528aa862e0a478d5aba2c7984e703a77d
leigh-perry <lperry.breakpoint@gmail.com>
2019-09-22 07:52:24 +0000

    master: v0.3.3 remove deliberate test failure

--- modules/core/src/test/scala/com/leighperry/conduction/config/ConfigSupportTest.scala
+++ modules/core/src/test/scala/com/leighperry/conduction/config/ConfigSupportTest.scala
@@ -145,7 +145,7 @@ object ConfigSupportTest
       } yield c
 
     io.unsafeRunSync()
-      .assertIs(ConfiguredError.MissingValue("MISSINGx").invalidNec)
+      .assertIs(ConfiguredError.MissingValue("MISSING").invalidNec)
   }
 
   test("Invalid valid Double") {
