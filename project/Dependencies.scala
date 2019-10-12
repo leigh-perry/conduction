18a33b25aa5695534132a7bb84f041ea5ba604e5
Scala Steward <me@scala-steward.org>
2019-10-12 10:45:26 +0200

    Update typesafe:config to 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val cats = "2.0.0"
     val catsEffect = "2.0.0"
 
-    val typesafeConfig = "1.3.4"
+    val typesafeConfig = "1.4.0"
 
     val minitest = "2.7.0"
     val scalacheck = "1.14.2"
